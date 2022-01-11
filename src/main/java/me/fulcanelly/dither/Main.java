c756cfc72c47f702abe7b50d9ade46635f9bc0b0
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-11 16:52:47 +0200

    fix swithching

--- src/main/java/me/fulcanelly/dither/Main.java
+++ src/main/java/me/fulcanelly/dither/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ImageHandler ih = null;
-        if (args.length >= 0 ) {
+        if (args.length > 0) {
             ih = new BayerDitheringFacade();
         } else {
             ih = new ErrorDiffusionFacade();
