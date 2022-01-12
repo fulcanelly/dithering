f08933753ccb593d6f13ff2ff39afbecdce35b4b
fulcanelly <dallasxfulcanelly@gmail.com>
2022-01-12 21:01:51 +0200

    add headers file

---
+++ cpp/headers.hpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics/Image.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <functional>
+#include <thread>
+#include <queue>
+#include <mutex>
